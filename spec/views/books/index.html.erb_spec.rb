8821cf272afc685cec3c466e741d522c653bc62c
Andrew Jorczak <andrew.jorczak@gmail.com>
2020-07-10 20:11:33 -0700

    Quick scaffold of books and adding rails assets.

---
+++ spec/views/books/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "books/index", type: :view do
+  before(:each) do
+    assign(:books, [
+      Book.create!(
+        title: "MyText",
+        author: "Author"
+      ),
+      Book.create!(
+        title: "MyText",
+        author: "Author"
+      )
+    ])
+  end
+
+  it "renders a list of books" do
+    render
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: "Author".to_s, count: 2
+  end
+end
