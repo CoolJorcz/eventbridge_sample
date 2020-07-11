eb5ec8e004cde04c4a187ec2b2a93b3bc0a6b8e5
Andrew Jorczak <andrew.jorczak@gmail.com>
2020-07-10 20:56:45 -0700

    Heroku setup debugging.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module EventbridgeService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Debugging for heroku
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
